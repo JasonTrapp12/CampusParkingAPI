dbd85470e1836ce5fa6b150b8e7ea1e707eee08e
Jason Trapp <jwt9552@g.rit.edu>
2023-11-03 10:31:06 -0400

    get req sends averages for each time now

--- main.py
+++ main.py
@@ -14,6 +14,7 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+
 class Review(BaseModel):
     id: Optional[int] = None
     lot: str
@@ -22,23 +23,41 @@ class Review(BaseModel):
 
 
 with open('reviews.json', 'r') as f:
-    reviews =  json.load(f)
+    reviews = json.load(f)
     f.close()
 
 
 def get_review(r_id: int):
     for review in reviews:
         if review['id'] == r_id:
-            return review     
+            return review
     return {}
+
 
 @app.get('/reviews/{lot_name}')
 def get_lot_reviews(lot_name: str):
-    lot_reviews = []
-    for review in reviews:
-        if review['lot'] == lot_name:
-            lot_reviews.append(review)
+    lot_reviews = {
+        "8-9AM": 0,
+        "9-10AM": 0,
+        "10-11AM": 0,
+        "11-12PM": 0,
+        "12-1PM": 0,
+        "1-2PM": 0,
+        "2-3PM": 0,
+        "3-4PM": 0
+    }
+
+    lot_reviews_keys = lot_reviews.keys()
+    for time in lot_reviews_keys:
+        num_reviews = 0
+        for review in reviews:
+            if review['lot'] == lot_name and review['time'] == time:
+                lot_reviews[time] += review['rating']
+                num_reviews += 1
+        if num_reviews > 0:
+            lot_reviews[time] = lot_reviews[time] / num_reviews
     return lot_reviews
+
 
 @app.post('/addReview', status_code=201)
 def post_review(new_review: Review):
@@ -49,10 +68,10 @@ def post_review(new_review: Review):
     id = max + 1
     print(id)
     review_to_add = {
-        "id" : id,
-        "lot" : new_review.lot,
-        "time" : new_review.time,
-        "rating" : new_review.rating
+        "id": id,
+        "lot": new_review.lot,
+        "time": new_review.time,
+        "rating": new_review.rating
     }
     reviews.append(review_to_add)
 
